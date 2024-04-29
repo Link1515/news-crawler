06ba481a796bcdd1ea98c8bae239bc2cf64e9927
Link1515 <h26281660@gmail.com>
2024-04-29 16:05:22 +0800

    add tailwindcss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     @foreach ($newsList as $mediaName => $popularNewsList)
-        <h1>{{ $mediaName }}</h1>
-            @foreach ($popularNewsList as $news)
-                <a href={{ $news->url }}>
-                    <img src={{ $news->image }} alt="image" />
-                    <h3>{{ $news->title }}</h3>
-                </a>
-            @endforeach
+        <h2>{{ $mediaName }}</h2>
+        @foreach ($popularNewsList as $news)
+            <a href={{ $news->url }}>
+                <img src={{ $news->image }} alt="image" />
+                <h3>{{ $news->title }}</h3>
+            </a>
+        @endforeach
     @endforeach
-</body>
-</html>
+@endsection
